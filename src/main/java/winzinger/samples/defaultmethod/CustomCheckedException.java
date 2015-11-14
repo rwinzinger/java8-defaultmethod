6ebf958f447674f6de579f64783e63fef533ee94
Winzinger, Ralph <Ralph.Winzinger@senacor.com>
2015-11-14 12:22:31 +0100

    oops ... build breaker :-(

--- src/main/java/winzinger/samples/defaultmethod/CustomCheckedException.java
+++ src/main/java/winzinger/samples/defaultmethod/CustomCheckedException.java
@@ -4,4 +4,8 @@ package winzinger.samples.defaultmethod;
  * Created by rwinzing on 14.11.15.
  */
 public class CustomCheckedException extends Exception implements CustomException {
+    @Override
+    public void setTraceId(String traceId) {
+
+    }
 }
--- src/main/java/winzinger/samples/defaultmethod/CustomUncheckedException.java
+++ src/main/java/winzinger/samples/defaultmethod/CustomUncheckedException.java
@@ -3,5 +3,9 @@ package winzinger.samples.defaultmethod;
 /**
  * Created by rwinzing on 14.11.15.
  */
-public class CustomUncheckedException extends RuntimeException implements CustomException{
+public class CustomUncheckedException extends RuntimeException implements CustomException {
+    @Override
+    public void setTraceId(String traceId) {
+
+    }
 }
